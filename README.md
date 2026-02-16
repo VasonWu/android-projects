c35eeab72eca1073a0c51b4f89b76d9ef55558b5
VasonWu <vasonwu@users.noreply.github.com>
2026-02-16 13:34:01 +0800

    Initial commit: HelloWorldApp and WebViewApp

---
+++ README.md
@@ -0,0 +1,8 @@
+# Android Projects
+
+Multiple Android projects in a single repository.
+
+## Projects
+
+- [HelloWorldApp](HelloWorldApp/) - Basic Hello World app
+- [WebViewApp](WebViewApp/) - WebView app (opens baidu.com)
---
+++ WebViewApp/app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.webviewapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        webView.loadUrl("https://www.baidu.com");
+        setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
