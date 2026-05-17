5e1a8ac860c0cb8a751b86a2fdd3bf71246601d6
Vason Wu <wushengwei@gmail.com>
2026-05-17 17:22:40 +0800

    Use TelephonyCallback instead of BroadcastReceiver for call monitoring

--- PhoneMonitorApp/app/src/main/java/com/example/phonemonitor/MonitorService.java
+++ PhoneMonitorApp/app/src/main/java/com/example/phonemonitor/MonitorService.java
@@ -8,9 +8,15 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Build;
 import android.os.IBinder;
+import android.telephony.TelephonyCallback;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
+
+import java.util.concurrent.Executor;
 
 public class MonitorService extends Service {
     private static final String TAG = "MonitorService";
@@ -18,7 +24,13 @@ public class MonitorService extends Service {
     private static final String CHANNEL_ID = "PhoneMonitorChannel";
 
     private NtfyClient ntfyClient;
+    private TelephonyManager telephonyManager;
+    private PhoneStateCallback phoneStateCallback;
+    private String lastPhoneNumber;
+    private boolean isRinging = false;
+    private boolean isOffhook = false;
 
+    @RequiresApi(api = Build.VERSION_CODES.S)
     @Override
     public void onCreate() {
         super.onCreate();
@@ -26,8 +38,16 @@ public class MonitorService extends Service {
 
         ntfyClient = new NtfyClient(this);
         SmsReceiver.setNtfyClient(ntfyClient);
-        PhoneStateReceiver.setNtfyClient(ntfyClient);
-        Log.i(TAG, "NtfyClient set for receivers");
+        Log.i(TAG, "NtfyClient set for SmsReceiver");
+
+        // Register TelephonyCallback for call state monitoring
+        telephonyManager = getSystemService(TelephonyManager.class);
+        if (telephonyManager != null) {
+            phoneStateCallback = new PhoneStateCallback();
+            Executor executor = getMainExecutor();
+            telephonyManager.registerTelephonyCallback(executor, phoneStateCallback);
+            Log.i(TAG, "TelephonyCallback registered");
+        }
 
         createNotificationChannel();
         startForeground(NOTIFICATION_ID, createNotification());
@@ -45,10 +65,69 @@ public class MonitorService extends Service {
         return null;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.S)
     @Override
     public void onDestroy() {
         super.onDestroy();
+        if (telephonyManager != null && phoneStateCallback != null) {
+            telephonyManager.unregisterTelephonyCallback(phoneStateCallback);
+            Log.i(TAG, "TelephonyCallback unregistered");
+        }
         Log.i(TAG, "Service destroyed");
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.S)
+    private class PhoneStateCallback extends TelephonyCallback implements TelephonyCallback.CallStateListener, TelephonyCallback.CallDisconnectCauseListener {
+        @Override
+        public void onCallStateChanged(int state) {
+            Log.i(TAG, "Phone state changed: " + state);
+            handleStateChange(state);
+        }
+
+        @Override
+        public void onCallDisconnectCauseChanged(int disconnectCause, int preciseDisconnectCause) {
+            Log.i(TAG, "Call disconnect cause: " + disconnectCause + ", precise: " + preciseDisconnectCause);
+        }
+    }
+
+    private void handleStateChange(int state) {
+        Log.i(TAG, "Handling state change: " + state + ", wasRinging=" + isRinging + ", wasOffhook=" + isOffhook);
+
+        switch (state) {
+            case TelephonyManager.CALL_STATE_RINGING:
+                // Incoming call - we don't get phone number from TelephonyCallback in API 31+
+                Log.i(TAG, "Incoming call detected");
+                isRinging = true;
+                isOffhook = false;
+                if (ntfyClient != null) {
+                    ntfyClient.sendIncomingCall("来电");
+                }
+                break;
+
+            case TelephonyManager.CALL_STATE_OFFHOOK:
+                if (isRinging) {
+                    // Call answered
+                    Log.i(TAG, "Call answered");
+                }
+                isOffhook = true;
+                break;
+
+            case TelephonyManager.CALL_STATE_IDLE:
+                if (isRinging) {
+                    // Missed call
+                    Log.i(TAG, "Missed call detected");
+                    if (ntfyClient != null) {
+                        ntfyClient.sendMissedCall("未接来电");
+                    }
+                } else if (isOffhook) {
+                    // Call ended
+                    Log.i(TAG, "Call ended");
+                    if (ntfyClient != null) {
+                        ntfyClient.sendCallEnded("通话结束", 0);
+                    }
+                }
+                break;
+        }
     }
 
     private void createNotificationChannel() {
